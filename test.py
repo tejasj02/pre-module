30e14bdbcc7b27f8d50811a0cbf525b0efedf63d
Tejas Jyothi <130318505+tejasj02@users.noreply.github.com>
2024-08-25 20:05:16 -0400

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-def test_length(d):
-    '''checks that only 1 flower is being returned'''
-    assert len(d) == 1
-
-def test_type(d):
-    '''checks that petal length is decimal value'''
-    assert isinstance(d[0], float)
